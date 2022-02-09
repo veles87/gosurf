0e71485ef29e311fa0d26f8763fadc2e722c0fa6
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-02-09 20:51:00 +0200

    ccc

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,8 @@ $(function(){
         speed: 500,
         fade: true,
         cssEase: 'linear',
+        prevArrow: '<img class"slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt="">',
+        nextArrow: '<img class"slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt="">',
+
     });
 });
