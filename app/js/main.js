87036f0a0cb334d79631a970143dc1c3ba1514ea
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-02-16 16:50:07 +0200

    add surf section

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,10 @@ $(function(){
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt="">',
+    });
 });
