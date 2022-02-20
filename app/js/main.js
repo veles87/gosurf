440fc3a8464ee4b1d39c1193e51694aa1b2c885b
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-02-20 20:55:39 +0200

    m

--- app/js/main.js
+++ app/js/main.js
@@ -27,4 +27,12 @@ $(function(){
         asNavFor: '.surf-slider',
         focusOnSelect: true,
     });
+    $('.travel__slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt="">',
+    });
 });
