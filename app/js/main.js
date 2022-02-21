20c684b9252e3e174fa8700635a483b4ffb8ef10
Maksim Chekhovskiy <veles.cafe@gmail.com>
2022-02-21 20:59:35 +0200

    add the travel and sleep sections

--- app/js/main.js
+++ app/js/main.js
@@ -35,4 +35,12 @@ $(function(){
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt="">',
     });
+    $('.sleep__slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/svg/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/svg/arrow-right.svg" alt="">',
+    });
 });
